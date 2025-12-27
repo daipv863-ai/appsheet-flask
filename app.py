0c86cb9bcca5a799f6f3788b9c70e50d95a2c49a
daipv863-ai <daipv863@gmail.com>
2025-12-27 14:50:55 +0700

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Route test
+@app.route("/")
+def home():
+    return "Flask is running OK"
+
+# Route cho AppSheet
 @app.route("/appsheet", methods=["POST"])
 def appsheet():
     data = request.json
